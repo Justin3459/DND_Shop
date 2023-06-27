c2194f2c3f934230409ff324e4644ddb0736d8af
Justin <justin3459@hotmail.com>
2023-06-27 12:55:02 -0400

    deleted handleCard from weapons component

--- dnd_store/src/components/Weapons.js
+++ dnd_store/src/components/Weapons.js
@@ -3,18 +3,6 @@ import Card from "./Card";
 
 
 function Weapons(weapons) {
-    // console.log(weapons.weapons)
-    // const weaponArray = weapons.weapons
-    // const handleCard = weaponArray.map((item) => 
-    // <div key={item.id} className="card">
-    //     <h1 className="cardName">{item.name}</h1>
-    //     <p>Cost: {item.cost}</p>
-    //     <p>Damage: {item.damage}</p>
-    //     <p>weight: {item.weight}</p>
-    //     <p>properties: {item.properties}</p>
-    //     <p>type: {item.type}</p>
-    // </div>
-    // ) 
 return (
     <>
         <Card items={weapons}/>
