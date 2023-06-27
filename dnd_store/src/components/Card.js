62085ccc7fddf31279f8c35ada2470cf7f49bef3
Justin <justin3459@hotmail.com>
2023-06-27 12:53:59 -0400

    create Card component. remove handleCard from weapons

---
+++ dnd_store/src/components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Card(items) {
+  const weaponArray = items.items.weapons;
+  console.log(weaponArray);
+  const handleCard = weaponArray.map((item) => (
+    <div key={item.id} className="card">
+      <h1 className="cardName">{item.name}</h1>
+      <p>Cost: {item.cost}</p>
+      <p>Damage: {item.damage}</p>
+      <p>weight: {item.weight}</p>
+      <p>properties: {item.properties}</p>
+      <p>type: {item.type}</p>
+    </div>
+  ));
+
+  return (
+    <>
+      {handleCard}
+    </>
+  );
+}
+
+export default Card;
--- dnd_store/src/components/Weapons.js
+++ dnd_store/src/components/Weapons.js
@@ -1,23 +1,23 @@
 import React from "react";
-
+import Card from "./Card";
 
 
 function Weapons(weapons) {
     // console.log(weapons.weapons)
-    const weaponArray = weapons.weapons
-    const handleCard = weaponArray.map((item) => 
-    <div key={item.id} className="card">
-        <h1 className="cardName">{item.name}</h1>
-        <p>Cost: {item.cost}</p>
-        <p>Damage: {item.damage}</p>
-        <p>weight: {item.weight}</p>
-        <p>properties: {item.properties}</p>
-        <p>type: {item.type}</p>
-    </div>
-    ) 
+    // const weaponArray = weapons.weapons
+    // const handleCard = weaponArray.map((item) => 
+    // <div key={item.id} className="card">
+    //     <h1 className="cardName">{item.name}</h1>
+    //     <p>Cost: {item.cost}</p>
+    //     <p>Damage: {item.damage}</p>
+    //     <p>weight: {item.weight}</p>
+    //     <p>properties: {item.properties}</p>
+    //     <p>type: {item.type}</p>
+    // </div>
+    // ) 
 return (
     <>
-        <h1>{handleCard}</h1>
+        <Card items={weapons}/>
     </>
   );
 }
