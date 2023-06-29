32d427c63a0f8e759c5b921502366a302249b419
Justin <justin3459@hotmail.com>
2023-06-29 11:53:40 -0400

    add to form in new.js

--- dnd_store/src/components/New.js
+++ dnd_store/src/components/New.js
@@ -1,35 +1,43 @@
-import React from 'react'
+import React from "react";
 
 function New() {
-
-    // const onSubmit = (id)=> fetch(`https://localhost:3000/${id}`, {Method:POST}).then(r=>r.json()).then()
+  // const onSubmit = (id)=> fetch(`https://localhost:3000/${id}`, {Method:POST}).then(r=>r.json()).then()
   return (
-   
-    <form> 
-        e.preventDefault()
-        <p>Name:
-        <input></input>
-        </p>
-        <p>Cost:
-        <input></input>
-        </p><p>Damage:
-        <input></input>
-        </p><p>Weight:
-        <input></input>
-        </p><p>Properties:
-        <input></input>
-        </p>
-        <div>Type:
-            <ul>
-                <input type="radio" value="simple" name="type"/>Simple Weapon
-            </ul>
-            <ul>
-                <input type="radio" value="martial" name="type"/>Martial Weapon
-            </ul>
-        </div>
-        {/* <button onClick= {()=>handleSubmit(task.id)}>Submit</button> */}
+    <form>
+      <label>
+        Name:
+        <input name="name"></input>
+      </label>
+      <label>
+        Cost:
+        <input name="cost"></input>
+      </label>
+      <label>
+        Damage:
+        <input name="damage"></input>
+      </label>
+      <label>
+        Weight:
+        <input name="weight"></input>
+      </label>
+      <label>
+        Properties:
+        <textarea name="properties"></textarea>
+      </label>
+      <label>
+        Type:
+        <select>
+          <option>Simple Weapon</option>
+          <option>Martial Weapon</option>
+          <option>Light Armor</option>
+          <option>Medium Armor</option>
+          <option>Heavy Armor</option>
+        </select>
+      </label>
+      <button>Submit</button>
+      {/* <button onClick= {()=>handleSubmit(task.id)}>Submit</button> */}
     </form>
-  )
+  );
 }
 
-export default New
+export default New;
